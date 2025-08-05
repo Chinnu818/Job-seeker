f4d7271b180c81109e8364390098960c0b49842e
Ramesh chavali <ramesh_chavali@srmap.edu.in>
2025-08-05 16:11:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -98,9 +98,9 @@ io.on('connection', (socket) => {
 
 // Frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
   });
 } else {
   app.use('*', (req, res) => {
